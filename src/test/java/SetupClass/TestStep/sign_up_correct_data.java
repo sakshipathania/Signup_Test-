e1060ee5a1718470d8a894c966cacf7e1ef14748
sakshipathania <62547853+sakshipathania@users.noreply.github.com>
2021-11-11 15:56:29 +0530

    Update sign_up_correct_data.java
    add the code for "verifyDeleteAccount".

--- src/test/java/SetupClass/TestStep/sign_up_correct_data.java
+++ src/test/java/SetupClass/TestStep/sign_up_correct_data.java
@@ -197,6 +197,10 @@ public class sign_up_correct_data extends Set {
 		js.executeScript("arguments[0].scrollIntoView();",delete_profile_coupon);
 		delete_profile_coupon.click();
 		 Thread.sleep(3000);
+		String verifyDeleteAccount = wait
+				.until(ExpectedConditions.elementToBeClickable(By.xpath("//div[@data-bind='html: $parent.prepareMessageForHtml(message.text)']"))).getText();
+	     Assert.assertTrue("Account is not deleted", verifyDeleteAccount.contains("Your account has been deleted successfully."));
+	     System.out.println("your account delete successfully");
 	}
 	
 	
