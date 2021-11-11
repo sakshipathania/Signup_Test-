3076c24ea03fc759f24c52c8d6fed154949ca3a9
sakshipathania <62547853+sakshipathania@users.noreply.github.com>
2021-11-11 16:10:45 +0530

    Update sign_up_correct_data.java

--- src/test/java/SetupClass/TestStep/sign_up_correct_data.java
+++ src/test/java/SetupClass/TestStep/sign_up_correct_data.java
@@ -131,8 +131,12 @@ public class sign_up_correct_data extends Set {
 	public void user_click_on_sign_up_button_to_sign_up_cd() throws InterruptedException  {
 	    
 		WebElement new_btn_signup = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector(".submit")));
-		Thread.sleep(2000);
+		Thread.sleep(1000);
 	    new_btn_signup.click();
+			Thread.sleep(1000);
+		String free_ppt_btn1 = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//div[contains(text(),'Thank you for registering with SlideTeam.')]"))).getText();
+		Assert.assertTrue("user is not able to signup", free_ppt_btn1.contains("Thank you for registering with SlideTeam."));
+		System.out.println("signup successfully");
 		Thread.sleep(2000);
 	   
 	}
