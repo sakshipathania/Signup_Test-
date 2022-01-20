613b8ca0f111b0fa6b900e53d7fc7527502b471b
sakshipathania <62547853+sakshipathania@users.noreply.github.com>
2022-01-20 17:49:05 +0530

    Update sign_up_correct_data.java

--- src/test/java/SetupClass/TestStep/sign_up_correct_data.java
+++ src/test/java/SetupClass/TestStep/sign_up_correct_data.java
@@ -242,7 +242,7 @@ public class sign_up_correct_data extends Set {
 		WebElement delete_profile_coupon = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("#flipModal > div > div > div.modal-footer.button_action > button.btn.btn-default.button_2")));
 		js.executeScript("arguments[0].scrollIntoView();",delete_profile_coupon);
 		delete_profile_coupon.click();
-		 Thread.sleep(9000);
+		Thread.sleep(30000);
 		String verifyDeleteAccount = wait
 				.until(ExpectedConditions.elementToBeClickable(By.xpath("//div[@data-bind='html: $parent.prepareMessageForHtml(message.text)']"))).getText();
 		 Thread.sleep(3000);
